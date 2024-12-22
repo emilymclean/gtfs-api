9f767629cffe3bb804f211e9eb347ed23a77bca2
Emily McLean <emilymcleandeveloper@gmail.com>
2024-12-22 14:43:56 +1100

    Update graph to consider directions of a route as distinct

--- script/gen/raptor/raptor_graph_generator.py
+++ script/gen/raptor/raptor_graph_generator.py
@@ -3,7 +3,7 @@ import math
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from pathlib import Path
-from typing import List, Dict, Set
+from typing import List, Dict, Set, Tuple
 
 from google.protobuf.json_format import MessageToJson
 
@@ -63,16 +63,20 @@ class NetworkGraphGenerator(Writer):
         self.stop_id_to_node_index = {}
 
         self.route_ids = []
-        self.stop_id_route_id_to_node_index = {}
+        self.stop_id_route_id_to_node_index: Dict[Tuple[int, str, int], int] = {}
 
         self.service_ids = []
         self.service_id_to_service_index = {}
 
-        self.trip_stop_to_route_edges: Dict[int, Dict[int, TripRouteEdge]] = {}
+        self.headings = []
+        self.heading_to_heading_index = {}
+
+        self.trip_stop_to_route_edges: Dict[int, Dict[Tuple[int, int], TripRouteEdge]] = {}
 
         self.nodes = []
 
     def _create_graph(self) -> pb.Graph:
+        self._compute_headings()
         self._generate_stop_nodes()
         self._connect_stops_by_transfer()
         self._generate_route_nodes()
@@ -98,6 +102,9 @@ class NetworkGraphGenerator(Writer):
         for s in self.service_ids:
             out.mappings.serviceIds.append(s)
 
+        for h in self.headings:
+            out.mappings.headings.append(h)
+
         return out
 
     def generate(self, output_folder: Path):
@@ -119,7 +126,8 @@ class NetworkGraphGenerator(Writer):
                 'nodeId': i,
                 'type': node_types[node.type],
                 'stopId': self.stop_ids[node.stopId],
-                'routeId': self.route_ids[node.routeId] if node.routeId is not None else None
+                'routeId': self.route_ids[node.routeId] if node.routeId is not None else None,
+                'heading': self.headings[node.headingId] if node.headingId is not None else None,
             }
             for edge in node.edges:
                 out['edges'].append(
@@ -134,6 +142,14 @@ class NetworkGraphGenerator(Writer):
                 )
 
         self._write(json.dumps(out, indent=2), output_folder.joinpath("network_graph.json"))
+
+    def _compute_headings(self):
+        for trip in self.trips:
+            if trip.trip_headsign in self.heading_to_heading_index:
+                continue
+
+            self.heading_to_heading_index[trip.trip_headsign] = len(self.headings)
+            self.headings.append(trip.trip_headsign)
 
     def _add_all_edges(self):
         print("Adding all edges")
@@ -154,24 +170,27 @@ class NetworkGraphGenerator(Writer):
             for i, stop_time in enumerate(stop_times):
                 stop_index = self.stop_id_to_node_index[stop_time.stop_id]
                 route_id = trip.route_id
+                heading_index = self.heading_to_heading_index[trip.trip_headsign]
 
-                if (stop_index, route_id) not in self.stop_id_route_id_to_node_index:
+                if (stop_index, route_id, heading_index) not in self.stop_id_route_id_to_node_index:
                     route_node = self._create_route_node(
                         self.route_index[route_id],
+                        heading_index,
                         stop_index
                     )
                     self.nodes.append(route_node.node)
                 else:
-                    index = self.stop_id_route_id_to_node_index[(stop_index, route_id)]
+                    index = self.stop_id_route_id_to_node_index[(stop_index, route_id, heading_index)]
                     route_node = NodeAndIndex(self.nodes[index], index)
 
-                self._create_stop_to_route_edge(stop_index, route_node.index, trip.service_id)
+                self._create_stop_to_route_edge(stop_index, route_node.index, heading_index, trip.service_id)
 
                 if i - 1 >= 0 and stop_time.stop_sequence > 0:
                     previous_stop_time = stop_times[i - 1]
                     previous_route_index = self.stop_id_route_id_to_node_index[(
                         self.stop_id_to_node_index[previous_stop_time.stop_id],
-                        route_id
+                        route_id,
+                        heading_index
                     )]
                     previous_departure_time = self.time_helper.output_time_seconds(previous_stop_time.departure_time)
                     if previous_route_index == route_node.index:
@@ -238,16 +257,18 @@ class NetworkGraphGenerator(Writer):
     def _create_route_node(
             self,
             route: RouteCSV,
+            heading_index: int,
             stop_index: int
     ) -> NodeAndIndex:
         route_index = len(self.route_ids)
         self.route_ids.append(route.id)
-        self.stop_id_route_id_to_node_index[(stop_index, route.id)] = route_index
+        self.stop_id_route_id_to_node_index[(stop_index, route.id, heading_index)] = route_index
 
         out = pb.Node()
         out.type = pb.NodeType.NODE_TYPE_STOP_ROUTE
         out.stopId = stop_index
         out.routeId = route_index
+        out.headingId = heading_index
 
         return NodeAndIndex(out, route_index)
 
@@ -269,6 +290,7 @@ class NetworkGraphGenerator(Writer):
             self,
             stop_index: int,
             route_index: int,
+            heading_index: int,
             service_id: str,
     ):
         service_index = self._register_service(service_id)
@@ -278,13 +300,14 @@ class NetworkGraphGenerator(Writer):
         else:
             stop_edges = {}
 
-        if route_index in stop_edges:
-            edge = stop_edges[route_index]
+        k = (route_index, heading_index)
+        if k in stop_edges:
+            edge = stop_edges[k]
             edge.available_services.add(service_index)
         else:
             edge = TripRouteEdge(route_index, {service_index})
 
-        stop_edges[route_index] = edge
+        stop_edges[k] = edge
         self.trip_stop_to_route_edges[stop_index] = stop_edges
 
     def _create_trip_edge(
