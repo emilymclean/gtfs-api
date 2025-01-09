8e1cce5c3572b3a293e2c9ee1f701934876d0981
Emily McLean <emilymcleandeveloper@gmail.com>
2025-01-09 11:53:34 +1100

    Correctly render platform content for protobuf

--- render-content.sh
+++ render-content.sh
@@ -1,6 +1,6 @@
 render () {
     platform=$1 pkl eval content/content.pkl -f json -o content/content$2.json
-    pkl eval content/protobuf-render.pkl  -o content/content.Pages$2.textpb
+    platform=$1 pkl eval content/protobuf-render.pkl  -o content/content.Pages$2.textpb
     protoc --encode=content.Pages content/format.proto < content/content.Pages$2.textpb > content/content$2.pb
     sha256sum content/content$2.pb | cut -d " " -f 1 > content/content.pb$2.sha
     sha256sum content/content$2.json | cut -d " " -f 1 > content/content.json$2.sha
