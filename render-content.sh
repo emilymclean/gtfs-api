c9e42c7d955b625e21dc7e25265b35fd0933047a
Emily McLean <emilymcleandeveloper@gmail.com>
2025-01-09 11:39:56 +1100

    Render platform specific variants of content
    Thanks apple you fucks

--- render-content.sh
+++ render-content.sh
@@ -1,5 +1,11 @@
-pkl eval content/content.pkl -f json -o content/content.json
-pkl eval content/protobuf-render.pkl  -o content/content.Pages.textpb
-protoc --encode=content.Pages content/format.proto < content/content.Pages.textpb > content/content.pb
-sha256sum content/content.pb | cut -d " " -f 1 > content/content.pb.sha
-sha256sum content/content.json | cut -d " " -f 1 > content/content.json.sha
+render () {
+    platform=$1 pkl eval content/content.pkl -f json -o content/content$2.json
+    pkl eval content/protobuf-render.pkl  -o content/content.Pages$2.textpb
+    protoc --encode=content.Pages content/format.proto < content/content.Pages$2.textpb > content/content$2.pb
+    sha256sum content/content$2.pb | cut -d " " -f 1 > content/content.pb$2.sha
+    sha256sum content/content$2.json | cut -d " " -f 1 > content/content.json$2.sha
+}
+
+render
+render ios ".ios"
+render android ".android"
