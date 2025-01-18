5b5381a46dc8f847012182a34e8df592df4a1e96
Emily McLean <emilymcleandeveloper@gmail.com>
2025-01-18 23:24:01 +1100

    Add journey config config type

--- render-content.sh
+++ render-content.sh
@@ -6,8 +6,12 @@ render () {
     local encode="$5"
     local proto="$6"
 
-    pkl eval "${jsonInput}" -f json -o "${outputName}${extension}.json"
-    pkl eval "${pklInput}" -o "${outputName}${extension}.textpb"
+    if [ -z "${pklInput}" ]; then
+        outputName="${outputName}" pkl eval "${jsonInput}" -m .
+    else
+        pkl eval "${jsonInput}" -f json -o "${outputName}${extension}.json"
+        pkl eval "${pklInput}" -o "${outputName}${extension}.textpb"
+    fi
     protoc "--encode=${encode}" "$proto" < "${outputName}${extension}.textpb" > "${outputName}${extension}.pb"
     sha256sum "${outputName}${extension}.pb" | cut -d " " -f 1 > "${outputName}${extension}.pb.sha"
     sha256sum "${outputName}${extension}.json" | cut -d " " -f 1 > "${outputName}${extension}.json.sha"
@@ -16,9 +20,11 @@ render () {
 }
 
 renderContent () {
-    platform=$1 render "content/output/content" "content/content.pkl" "content/protobuf-render.pkl" "$2" "content.Pages" "content/format.proto"
+    platform=$1 render "build/canberra/v1/content" "content/content.pkl" "content/protobuf-render.pkl" "$2" "content.Pages" "content/format.proto"
 }
 
 renderContent
 renderContent ios ".ios"
 renderContent android ".android"
+
+render "build/canberra/v1/journey-config" "config/journey-config.pkl" "" "" "proto.JourneySearchConfigEndpoint" "script/format.proto"
