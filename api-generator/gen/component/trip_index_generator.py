11cb7d17859de29049ca90de9ac31e81b1327cde
Emily McLean <emilymcleandeveloper@gmail.com>
2025-07-04 20:28:30 +1000

    Implement a better trip-index

--- api-generator/gen/component/trip_index_generator.py
+++ api-generator/gen/component/trip_index_generator.py
@@ -16,16 +16,16 @@ class TripInformation:
 
 
 @dataclass
+class AssociatedTrips:
+    parent_id: str
+    trip_ids: List[str]
+
+
+@dataclass
 class TripIndexIntermediary:
     information: List[TripInformation]
-
-
-class JsonTripIndexGeneratorFormat(JsonGeneratorFormat[TripIndexIntermediary]):
-
-    def parse(self, intermediary: TripIndexIntermediary, distinguisher: Optional[str]) -> Any:
-        return {
-            'route': intermediary.to_json()
-        }
+    route_associated_trips: List[AssociatedTrips]
+    stop_associated_trips: List[AssociatedTrips]
 
 
 class ProtoTripIndexGeneratorFormat(ProtoGeneratorFormat[TripIndexIntermediary]):
@@ -41,6 +41,12 @@ class ProtoTripIndexGeneratorFormat(ProtoGeneratorFormat[TripIndexIntermediary])
             info.stopIds.extend(trip.stop_ids)
 
             index.trips.append(info)
+
+        for r in intermediary.route_associated_trips:
+            index.tripsByRoute[r.parent_id].tripId.extend(r.trip_ids)
+
+        for s in intermediary.stop_associated_trips:
+            index.tripsByStop[s.parent_id].tripId.extend(s.trip_ids)
 
         return index
 
@@ -70,22 +76,44 @@ class TripIndexGeneratorComponent(FormatGeneratorComponent[TripIndexIntermediary
         return output_folder.joinpath(f"trip-index.{extension}")
 
     def _read_intermediary(self, distinguisher: Optional[str]) -> List[TripIndexIntermediary]:
-        return [TripIndexIntermediary(
-            [
+        data = flatten_parsed(filter_parsed_by_distinguisher(self.trip_data, distinguisher))
+
+        by_route = {}
+        by_stop = {}
+        information = []
+
+        for trip in data:
+            if trip.route_id not in by_route:
+                by_route[trip.route_id] = [trip.id]
+            else:
+                by_route[trip.route_id].append(trip.id)
+
+            associated_stops = list({
+                xs
+                for s in self.stop_time_index_by_trip[trip.id]
+                for xs in self._stops_and_parents(s.stop_id)
+            })
+            for s in associated_stops:
+                if s not in by_stop:
+                    by_stop[s] = [trip.id]
+                else:
+                    by_stop[s].append(trip.id)
+
+            information.append(
                 TripInformation(
-                    t.id,
-                    t.route_id,
-                    list(
-                        {
-                            xs
-                            for s in self.stop_time_index_by_trip[t.id]
-                            for xs in self._stops_and_parents(s.stop_id)
-                        }
-                    )
+                    trip.id,
+                    trip.route_id,
+                    associated_stops
                 )
-                for t in flatten_parsed(filter_parsed_by_distinguisher(self.trip_data, distinguisher))
-            ]
-        )]
+            )
+
+        return [
+            TripIndexIntermediary(
+                [],
+                list(map(lambda x: AssociatedTrips(x[0], x[1]), by_route.items())),
+                list(map(lambda x: AssociatedTrips(x[0], x[1]), by_stop.items())),
+            )
+        ]
 
     def _stops_and_parents(self, stop_id: str) -> List[str]:
         out = [stop_id]
--- api-generator/gen/trip_index_pb2.py
+++ api-generator/gen/trip_index_pb2.py
@@ -24,7 +24,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x10trip-index.proto\x12\ntrip_index\"7\n\tTripIndex\x12*\n\x05trips\x18\x01 \x03(\x0b\x32\x1b.trip_index.TripInformation\"C\n\x0fTripInformation\x12\x0e\n\x06tripId\x18\x01 \x02(\t\x12\x0f\n\x07routeId\x18\x02 \x02(\t\x12\x0f\n\x07stopIds\x18\x03 \x03(\tB\x1a\n\x18\x63l.emilym.gtfs.tripindex')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x10trip-index.proto\x12\ntrip_index\"\xd6\x02\n\tTripIndex\x12*\n\x05trips\x18\x01 \x03(\x0b\x32\x1b.trip_index.TripInformation\x12=\n\x0ctripsByRoute\x18\x02 \x03(\x0b\x32\'.trip_index.TripIndex.TripsByRouteEntry\x12;\n\x0btripsByStop\x18\x03 \x03(\x0b\x32&.trip_index.TripIndex.TripsByStopEntry\x1aP\n\x11TripsByRouteEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12*\n\x05value\x18\x02 \x01(\x0b\x32\x1b.trip_index.AssociatedTrips:\x02\x38\x01\x1aO\n\x10TripsByStopEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12*\n\x05value\x18\x02 \x01(\x0b\x32\x1b.trip_index.AssociatedTrips:\x02\x38\x01\"C\n\x0fTripInformation\x12\x0e\n\x06tripId\x18\x01 \x02(\t\x12\x0f\n\x07routeId\x18\x02 \x02(\t\x12\x0f\n\x07stopIds\x18\x03 \x03(\t\"!\n\x0f\x41ssociatedTrips\x12\x0e\n\x06tripId\x18\x01 \x03(\tB\x1a\n\x18\x63l.emilym.gtfs.tripindex')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
@@ -32,8 +32,18 @@ _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'trip_index_pb2', _globals)
 if not _descriptor._USE_C_DESCRIPTORS:
   _globals['DESCRIPTOR']._loaded_options = None
   _globals['DESCRIPTOR']._serialized_options = b'\n\030cl.emilym.gtfs.tripindex'
-  _globals['_TRIPINDEX']._serialized_start=32
-  _globals['_TRIPINDEX']._serialized_end=87
-  _globals['_TRIPINFORMATION']._serialized_start=89
-  _globals['_TRIPINFORMATION']._serialized_end=156
+  _globals['_TRIPINDEX_TRIPSBYROUTEENTRY']._loaded_options = None
+  _globals['_TRIPINDEX_TRIPSBYROUTEENTRY']._serialized_options = b'8\001'
+  _globals['_TRIPINDEX_TRIPSBYSTOPENTRY']._loaded_options = None
+  _globals['_TRIPINDEX_TRIPSBYSTOPENTRY']._serialized_options = b'8\001'
+  _globals['_TRIPINDEX']._serialized_start=33
+  _globals['_TRIPINDEX']._serialized_end=375
+  _globals['_TRIPINDEX_TRIPSBYROUTEENTRY']._serialized_start=214
+  _globals['_TRIPINDEX_TRIPSBYROUTEENTRY']._serialized_end=294
+  _globals['_TRIPINDEX_TRIPSBYSTOPENTRY']._serialized_start=296
+  _globals['_TRIPINDEX_TRIPSBYSTOPENTRY']._serialized_end=375
+  _globals['_TRIPINFORMATION']._serialized_start=377
+  _globals['_TRIPINFORMATION']._serialized_end=444
+  _globals['_ASSOCIATEDTRIPS']._serialized_start=446
+  _globals['_ASSOCIATEDTRIPS']._serialized_end=479
 # @@protoc_insertion_point(module_scope)
